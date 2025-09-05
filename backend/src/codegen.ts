2f7b76b6b5b49ce28040327663f339ac7e6d2fb0
Shawn <34017019+shassen@users.noreply.github.com>
2025-09-04 20:52:29 -0400

    Turbo installed and working, eslint configured. (#36)

--- backend/src/codegen.ts
+++ backend/src/codegen.ts
@@ -1,14 +1,3 @@
-// import "reflect-metadata"
-// import { buildSchema } from "type-graphql"
-// import { TodoCustomResolver } from "./graphql/TodoCustomResolver"
-// import { UserCustomResolver } from "./graphql/UserCustomResolver"
-// import { CollectionCustomResolver } from "./graphql/CollectionCustomResolver"
-
-// const schema = buildSchema({
-//   resolvers: [TodoCustomResolver, UserCustomResolver, CollectionCustomResolver],
-//   emitSchemaFile: "src/schema.graphql",
-// })
-
 import { createSchema } from "./graphql/schema"
 
-const schema = createSchema("src/schema.graphql")
+createSchema("src/schema.graphql")
