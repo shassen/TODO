c27365636bee10df8054eda174673463265af201
Shawn Hassen <shassen89@gmail.com>
2025-09-19 23:18:57 -0400

    Add vercel analytics to project

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { Providers } from "../components/providers/Providers"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {children}
           <SpeedInsights />
         </Providers>
+        <Analytics />
       </body>
     </html>
   )
